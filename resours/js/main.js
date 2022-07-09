ae500be96a4c337349b5840615b6e10b37cf12f2
Md. Maidul islam <108537737+maidulhp@users.noreply.github.com>
2022-07-09 10:22:45 +0600

    Add files via upload

--- resours/js/main.js
+++ resours/js/main.js
@@ -11,17 +11,17 @@ $(document).ready(function(){
     var mixer = mixitup('.portfoilo-item');
 
     // SMOOTH SCROLL FOR ALL BROWSER
-    // $("a").on(|'click', function(event){
-    //     if(this.hash !== ""){
-    //         event.preventDefault();
-    //         var hash = this.hash;
-    //         $('html,body').animate({
-    //             scrollTop: $(hash).offset().top
-    //         }, 800, function(){
-    //             window.location.hash = hash;
-    //         });
-    //     }
-    // });
+        $("a").on('click', function(event){
+            if(this.hash !== ""){
+                event.preventDefault();
+                var hash = this.hash;
+                $('html,body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
 });
 
 function openNav(){
